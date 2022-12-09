6f7a2c9ab8758aa2cbf9f399e8626ff56808737c
Charles <charles.lamarque69@gmail.com>
2022-12-09 12:51:30 +0100

    Add div grid generation

---
+++ script.js
@@ -0,0 +1,14 @@
+createGrid();
+
+function createGrid() {
+    const container = document.querySelector(".gridContainer");
+    for (let i = 0; i < 16; i++) {
+        const newDiv = document.createElement("div");
+        container.appendChild(newDiv);
+        for (let j = 0; j < 16; j++) {
+            const newPixel = document.createElement('div');
+            newPixel.classList.add('pixel');
+            newDiv.appendChild(newPixel);
+        }
+    }
+}
