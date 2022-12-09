c19e883eccec75d2f4486aad731b7d53017d69bb
Charles <charles.lamarque69@gmail.com>
2022-12-09 14:15:08 +0100

    Add button to create a new custom grid

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-createGrid();
+createGrid(16, 16);
 
-function createGrid() {
+function createGrid(size) {
     const container = document.querySelector(".gridContainer");
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < size; i++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add('pixelLine');
         container.appendChild(newDiv);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < size; j++) {
             const newPixel = document.createElement('div');
             newPixel.classList.add('pixel');
+            newPixel.addEventListener("mouseenter", function(e) {
+                this.classList.toggle('hover');
+            })
+            newPixel.addEventListener("mouseleave", function(e) {
+                this.classList.toggle('hover');
+            })
+
             newDiv.appendChild(newPixel);
         }
     }
+}
+
+function pressNewGrid() {
+    let desiredSize = prompt("New grid size (max 100): ");
+
+    if (desiredSize > 100 || desiredSize < 1 || isNaN(desiredSize)) {
+        alert("Size must be within 1 and 100 included");
+        return;
+    }
+
+    let lines = document.querySelectorAll(".pixelLine");
+    lines.forEach(line => line.remove());
+
+    createGrid(desiredSize);
 }
