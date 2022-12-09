9b47a22bb5f813016348a351eb1b5488fc7c3d0f
Charles <charles.lamarque69@gmail.com>
2022-12-09 13:23:43 +0100

    Add grid layout and flex properties

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function createGrid() {
     const container = document.querySelector(".gridContainer");
     for (let i = 0; i < 16; i++) {
         const newDiv = document.createElement("div");
+        newDiv.classList.add('pixelLine');
         container.appendChild(newDiv);
         for (let j = 0; j < 16; j++) {
             const newPixel = document.createElement('div');
